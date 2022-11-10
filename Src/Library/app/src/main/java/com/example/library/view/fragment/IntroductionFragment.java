6d83b37b6947c8551f9447fe3b29092f4023de8c
Imcaicai <2939361916@qq.com>
2022-11-10 20:43:58 +0800

    20221110

--- Src/Library/app/src/main/java/com/example/library/view/fragment/IntroductionFragment.java
+++
@@ -1,4 +0,0 @@
-package com.example.library.view.fragment;
-
-public class IntroductionFragment {
-}
--- Src/Library/app/src/main/java/com/example/library/view/fragment/ScanCodeFragment.java
+++
@@ -1,7 +0,0 @@
-package com.example.library.view.fragment;
-
-import androidx.fragment.app.Fragment;
-
-public class ScanCodeFragment extends Fragment {
-
-}
